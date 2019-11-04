803fb4f8161cd3df8633fa4272321af3cc451329
Aman Mittal <amandeepmittal@live.com>
2019-11-04 19:53:17 +0530

    add remote PN controller component

--- App.js
+++ App.js
@@ -1,114 +1,11 @@
-// import React from 'react'
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-//   FlatList
-// } from 'react-native'
-// import PushController from './src/PushController'
-
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions
-// } from 'react-native/Libraries/NewAppScreen'
-
-// let pushData = [
-//   {
-//     title: 'First push',
-//     message: 'First push message'
-//   },
-//   {
-//     title: 'Second push',
-//     message: 'Second push message'
-//   }
-// ]
-
-// _renderItem = ({ item }) => (
-//   <View key={item.title}>
-//     <Text style={styles.title}>{item.title}</Text>
-//     <Text style={styles.message}>{item.message}</Text>
-//   </View>
-// )
-
-// const App = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle='dark-content' />
-//       <SafeAreaView>
-//         <View style={styles.scrollView}>
-//           <Header />
-//           <View style={styles.listHeader}>
-//             <Text>Push Notifications</Text>
-//           </View>
-//           <View style={styles.body}>
-//             <FlatList
-//               data={pushData}
-//               renderItem={item => this._renderItem(item)}
-//               keyExtractor={item => item.title}
-//             />
-//             {/* <LearnMoreLinks /> */}
-//           </View>
-//         </View>
-//       </SafeAreaView>
-//       <PushController />
-//     </>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   scrollView: { backgroundColor: Colors.lighter },
-//   listHeader: {
-//     backgroundColor: '#eee',
-//     color: '#222',
-//     height: 44,
-//     padding: 12
-//   },
-//   title: { fontSize: 18, fontWeight: 'bold', paddingTop: 10 },
-//   message: {
-//     fontSize: 14,
-//     paddingBottom: 15,
-//     borderBottomColor: '#ccc',
-//     borderBottomWidth: 1
-//   },
-//   engine: { position: 'absolute', right: 0 },
-//   body: {
-//     backgroundColor: Colors.white,
-//     paddingHorizontal: 20,
-//     paddingVertical: 10
-//   },
-//   sectionContainer: { marginTop: 32, paddingHorizontal: 24 },
-//   sectionTitle: { fontSize: 24, fontWeight: '600', color: Colors.black },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark
-//   },
-//   highlight: { fontWeight: '700' },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right'
-//   }
-// })
-
-// export default App
-
 import React from 'react'
 import { Text, View, Button, StyleSheet } from 'react-native'
 import {
   LocalNotification,
   ScheduledLocalNotification
 } from './src/services/LocalPushController'
+
+import RemotePushController from './src/services/RemotePushController'
 
 const App = () => {
   const handleButtonPress = () => {
@@ -131,6 +28,7 @@ const App = () => {
           onPress={handleScheduleNotification}
         />
       </View>
+      <RemotePushController />
     </View>
   )
 }
