59a50f07882d7e2632dbb645dd012912de39c99f
Aman Mittal <amandeepmittal@live.com>
2019-11-04 18:50:35 +0530

    local PNs and scheduled PNs

--- App.js
+++ App.js
@@ -1,114 +1,149 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+// import React from 'react'
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+//   FlatList
+// } from 'react-native'
+// import PushController from './src/PushController'
 
-import React from 'react';
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions
+// } from 'react-native/Libraries/NewAppScreen'
+
+// let pushData = [
+//   {
+//     title: 'First push',
+//     message: 'First push message'
+//   },
+//   {
+//     title: 'Second push',
+//     message: 'Second push message'
+//   }
+// ]
+
+// _renderItem = ({ item }) => (
+//   <View key={item.title}>
+//     <Text style={styles.title}>{item.title}</Text>
+//     <Text style={styles.message}>{item.message}</Text>
+//   </View>
+// )
+
+// const App = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle='dark-content' />
+//       <SafeAreaView>
+//         <View style={styles.scrollView}>
+//           <Header />
+//           <View style={styles.listHeader}>
+//             <Text>Push Notifications</Text>
+//           </View>
+//           <View style={styles.body}>
+//             <FlatList
+//               data={pushData}
+//               renderItem={item => this._renderItem(item)}
+//               keyExtractor={item => item.title}
+//             />
+//             {/* <LearnMoreLinks /> */}
+//           </View>
+//         </View>
+//       </SafeAreaView>
+//       <PushController />
+//     </>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   scrollView: { backgroundColor: Colors.lighter },
+//   listHeader: {
+//     backgroundColor: '#eee',
+//     color: '#222',
+//     height: 44,
+//     padding: 12
+//   },
+//   title: { fontSize: 18, fontWeight: 'bold', paddingTop: 10 },
+//   message: {
+//     fontSize: 14,
+//     paddingBottom: 15,
+//     borderBottomColor: '#ccc',
+//     borderBottomWidth: 1
+//   },
+//   engine: { position: 'absolute', right: 0 },
+//   body: {
+//     backgroundColor: Colors.white,
+//     paddingHorizontal: 20,
+//     paddingVertical: 10
+//   },
+//   sectionContainer: { marginTop: 32, paddingHorizontal: 24 },
+//   sectionTitle: { fontSize: 24, fontWeight: '600', color: Colors.black },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark
+//   },
+//   highlight: { fontWeight: '700' },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right'
+//   }
+// })
+
+// export default App
+
+import React from 'react'
+import { Text, View, Button, StyleSheet } from 'react-native'
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  LocalNotification,
+  ScheduledLocalNotification
+} from './src/services/LocalPushController'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const handleButtonPress = () => {
+    LocalNotification()
+  }
 
-const App: () => React$Node = () => {
+  // const handleScheduleNotification = () => {
+  //   ScheduledLocalNotification()
+  // }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <View style={styles.container}>
+      <Text>Press a button to trigger the notification</Text>
+      <View style={{ marginTop: 20 }}>
+        <Button title={'Local Push Notification'} onPress={handleButtonPress} />
+      </View>
+      {/* <View style={{ marginTop: 20 }}>
+        <Button
+          title={'Scheduled Local Push Notification'}
+          onPress={handleScheduleNotification}
+        />
+      </View> */}
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  buttonContainer: {
+    marginTop: 20
+  }
+})
 
-export default App;
+export default App
